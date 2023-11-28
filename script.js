a817711281eb56d759e93b0e516ded448b0f5c87
Avinash-Amudala <aa9429@g.rit.edu>
2023-11-28 17:27:46 -0500

    personalize issue solved!

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function initializeApp() {
         fetchCsvData('https://uniview-dynamodb.s3.us-east-2.amazonaws.com/cleaned_items_university_reviews.csv', processUniversityKeywords);
         fetchCsvData('https://uniview-dynamodb.s3.us-east-2.amazonaws.com/updated_interactions_dataset.csv', processUniversityReviews);
+        attachSearchEventHandler();
     }
 
     // Fetch CSV data from S3
@@ -13,9 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         Papa.parse(csvUrl, {
             download: true,
             header: true,
-            complete: function (results) {
-                callback(results.data);
-            }
+            complete: results => callback(results.data)
         });
     }
 
@@ -25,18 +24,10 @@ document.addEventListener('DOMContentLoaded', function () {
             if (!universitiesData[row.ITEM_ID]) {
                 universitiesData[row.ITEM_ID] = { keywords: new Set(), reviews: [] };
             }
-            if (row.KEYWORDS) {
-                row.KEYWORDS.split(',').forEach(keyword => {
-                    universitiesData[row.ITEM_ID].keywords.add(keyword.trim());
-                });
-            } else {
-                // Log for debugging
-                console.log('Row with undefined KEYWORDS:', row);
-            }
+            row.KEYWORDS?.split(',').forEach(keyword => universitiesData[row.ITEM_ID].keywords.add(keyword.trim()));
         });
-        displayUniversityCards(Object.keys(universitiesData));
+        displayUniversityCards();
     }
-
 
     // Process Reviews CSV Data
     function processUniversityReviews(data) {
@@ -48,277 +39,71 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Display university cards
-    function displayUniversityCards(universities) {
+    function displayUniversityCards(filteredNames = Object.keys(universitiesData)) {
         const universityList = document.getElementById('universityList');
         universityList.innerHTML = '';
-        universities.forEach(universityName => {
-            const card = document.createElement('div');
-            card.className = 'university-card';
-            card.textContent = universityName;
-            card.addEventListener('click', function () {
-                displayUniversityDetails(universityName);
-            });
+        filteredNames.forEach(name => {
+            const card = createUniversityCard(name);
             universityList.appendChild(card);
         });
     }
 
+    function createUniversityCard(name) {
+        const card = document.createElement('div');
+        card.className = 'university-card';
+        card.textContent = name;
+        card.onclick = () => displayUniversityDetails(name);
+        return card;
+    }
+
     // Display university details
-    function displayUniversityDetails(universityName) {
-        alert('Clicked on: ' + universityName);
-        const details = universitiesData[universityName];
-        const modal = createModal(universityName, details);
+    function displayUniversityDetails(name) {
+        const details = universitiesData[name];
+        const modal = createModal(name, details);
         document.body.appendChild(modal);
-        createSentimentChart(calculateSentimentCounts(details.reviews), 'sentimentChart');
-        modal.style.display = 'block';
-        console.log('Displaying details for:', universityName);
+        getPersonalizeRecommendations(name);
     }
 
     // Create Modal for University Details
-    function createModal(universityName, details) {
-        console.log('Creating modal for:', universityName);
+    function createModal(name, details) {
         const modal = document.createElement('div');
-        modal.className = 'modal';document.addEventListener('DOMContentLoaded', function () {
-            let universitiesData = {};
-            let reviewsData = {};
-
-            // Initialize the application
-            function initializeApp() {
-                fetchCsvData('https://uniview-dynamodb.s3.us-east-2.amazonaws.com/cleaned_items_university_reviews.csv', processUniversityKeywords);
-                fetchCsvData('https://uniview-dynamodb.s3.us-east-2.amazonaws.com/updated_interactions_dataset.csv', processUniversityReviews);
-            }
-
-            // Fetch CSV data from S3
-            function fetchCsvData(csvUrl, callback) {
-                Papa.parse(csvUrl, {
-                    download: true,
-                    header: true,
-                    complete: function (results) {
-                        callback(results.data);
-                    }
-                });
-            }
-
-            // Process Keywords CSV Data
-            function processUniversityKeywords(data) {
-                data.forEach(row => {
-                    if (!universitiesData[row.ITEM_ID]) {
-                        universitiesData[row.ITEM_ID] = { keywords: new Set(), reviews: [] };
-                    }
-                    if (row.KEYWORDS) {
-                        row.KEYWORDS.split(',').forEach(keyword => {
-                            universitiesData[row.ITEM_ID].keywords.add(keyword.trim());
-                        });
-                    } else {
-                        // Log for debugging
-                        console.log('Row with undefined KEYWORDS:', row);
-                    }
-                });
-                displayUniversityCards(Object.keys(universitiesData));
-            }
-
-
-            // Process Reviews CSV Data
-            function processUniversityReviews(data) {
-                reviewsData = data.reduce((acc, row) => {
-                    if (!acc[row.ITEM_ID]) acc[row.ITEM_ID] = [];
-                    acc[row.ITEM_ID].push(row);
-                    return acc;
-                }, {});
-            }
-
-            // Display university cards
-            function displayUniversityCards(universities) {
-                const universityList = document.getElementById('universityList');
-                universityList.innerHTML = '';
-                universities.forEach(universityName => {
-                    const card = document.createElement('div');
-                    card.className = 'university-card';
-                    card.textContent = universityName;
-                    card.addEventListener('click', function () {
-                        displayUniversityDetails(universityName);
-                    });
-                    universityList.appendChild(card);
-                });
-            }
-
-            // Display university details
-            function displayUniversityDetails(universityName) {
-                const details = universitiesData[universityName];
-                const modal = createModal(universityName, details);
-                document.body.appendChild(modal);
-                createSentimentChart(calculateSentimentCounts(details.reviews), 'sentimentChart');
-                getPersonalizeRecommendations(universityName); // Fetch Personalize data
-                modal.style.display = 'block';
-            }
-
-            // Create Modal for University Details
-            function createModal(universityName, details) {
-                const modal = document.createElement('div');
-                modal.className = 'modal';
-                modal.innerHTML = `
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <h2>${universityName}</h2>
-            <div class="content-container">
-                <div class="chart-container">
-                    <canvas id="sentimentChart"></canvas>
-                </div>
-                <div class="keywords-container">
-                    <h3>Recommended Universities</h3>
-                    <ul id="recommended-universities"></ul> <!-- Placeholder for recommended universities -->
-                </div>
-            </div>
-        </div>`;
-                addModalCloseEvents(modal);
-                return modal;
-            }
-
-            // Calculate Sentiment Counts
-            function calculateSentimentCounts(reviews) {
-                const sentimentCounts = { POSITIVE: 0, NEGATIVE: 0, NEUTRAL: 0, MIXED: 0 };
-                reviews.forEach(review => {
-                    sentimentCounts[review.EVENT_VALUE]++;
-                });
-                return sentimentCounts;
-            }
-
-            // Add Close Events to Modal
-            function addModalCloseEvents(modal) {
-                var span = modal.querySelector(".close");
-                span.onclick = function() { modal.style.display = "none"; modal.remove(); };
-                window.onclick = function(event) { if (event.target == modal) { modal.style.display = "none"; modal.remove(); } };
-            }
-            let currentChart;
-
-            function createSentimentChart(sentimentCounts, canvasId) {
-                const ctx = document.getElementById(canvasId).getContext('2d');
-
-                // Destroy the existing chart if it exists
-                if (currentChart) {
-                    currentChart.destroy();
-                }
-
-                // Create a new chart
-                currentChart = new Chart(ctx, {
-                    type: 'pie',
-                    data: {
-                        labels: ['Positive', 'Negative', 'Neutral', 'Mixed'],
-                        datasets: [{
-                            label: 'Sentiment Analysis',
-                            data: [sentimentCounts.POSITIVE, sentimentCounts.NEGATIVE, sentimentCounts.NEUTRAL, sentimentCounts.MIXED],
-                            backgroundColor: ['green', 'red', 'blue', 'gray'],
-                            borderColor: ['darkgreen', 'darkred', 'darkblue', 'darkgray'],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false
-                    }
-                });
-            }
-
-
-            // Calculate sentiment percentage (optional for tooltips)
-            function calculateSentimentPercentage(count, total) {
-                return ((count / total) * 100).toFixed(2);
-            }
-
-            // Personalize recommendations (optional)
-            function getPersonalizeRecommendations(userId) {
-                const apiGatewayUrl = 'https://za8k6zxf6c.execute-api.us-east-2.amazonaws.com/prod';
-                fetch(apiGatewayUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ userId: userId })
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        displayPersonalizeRecommendations(data, userId);
-                    })
-                    .catch(error => console.error('Error:', error));
-            }
-            function displayPersonalizeRecommendations(data, universityName) {
-                const recommendationList = document.getElementById('recommended-universities');
-                recommendationList.innerHTML = data.itemList.map(item => `<li>${item.itemId}</li>`).join('');
-                // Optionally, you can also display scores or any other relevant info from the data
-            }
-
-            function displayKeywords(data, universityName) {
-                // Assuming data structure is known and contains positiveKeywords and negativeKeywords arrays
-                const positiveList = document.getElementById('positive-keywords');
-                const negativeList = document.getElementById('negative-keywords');
-                positiveList.innerHTML = data.positiveKeywords.map(kw => `<li>${kw}</li>`).join('');
-                negativeList.innerHTML = data.negativeKeywords.map(kw => `<li>${kw}</li>`).join('');
-                document.querySelector('.modal').style.display = 'block'; // Display modal after updating keywords
-            }
-
-            // Event listener for the search bar
-            document.getElementById('searchBar').addEventListener('input', function (e) {
-                const searchTerm = e.target.value.toLowerCase();
-                const filteredUniversities = Object.keys(universitiesData).filter(universityName =>
-                    universityName.toLowerCase().includes(searchTerm)
-                );
-                displayUniversityCards(filteredUniversities);
-            });
-
-            // Initialize the application
-            initializeApp();
-        });
-
+        modal.className = 'modal';
         modal.innerHTML = `
             <div class="modal-content">
-                <span class="close">&times;</span>
-                <h2>${universityName}</h2>
+                <span class="close" onclick="this.parentElement.parentElement.style.display='none'">&times;</span>
+                <h2>${name}</h2>
                 <div class="content-container">
                     <div class="chart-container">
                         <canvas id="sentimentChart"></canvas>
                     </div>
                     <div class="keywords-container">
-                        <h3>Keywords</h3>
-                        <ul>${Array.from(details.keywords).map(keyword => `<li>${keyword}</li>`).join('')}</ul>
+                        <h3>Recommended Universities</h3>
+                        <ul id="recommended-universities"></ul>
                     </div>
                 </div>
             </div>`;
-        addModalCloseEvents(modal);
+        createSentimentChart(calculateSentimentCounts(details.reviews), 'sentimentChart');
         return modal;
     }
 
     // Calculate Sentiment Counts
     function calculateSentimentCounts(reviews) {
-        const sentimentCounts = { POSITIVE: 0, NEGATIVE: 0, NEUTRAL: 0, MIXED: 0 };
-        reviews.forEach(review => {
-            sentimentCounts[review.EVENT_VALUE]++;
-        });
-        return sentimentCounts;
+        return reviews.reduce((counts, review) => {
+            counts[review.EVENT_VALUE] = (counts[review.EVENT_VALUE] || 0) + 1;
+            return counts;
+        }, { POSITIVE: 0, NEGATIVE: 0, NEUTRAL: 0, MIXED: 0 });
     }
 
-    // Add Close Events to Modal
-    function addModalCloseEvents(modal) {
-        var span = modal.querySelector(".close");
-        span.onclick = function() { modal.style.display = "none"; modal.remove(); };
-        window.onclick = function(event) { if (event.target == modal) { modal.style.display = "none"; modal.remove(); } };
-    }
-    let currentChart;
-
-    function createSentimentChart(sentimentCounts, canvasId) {
+    // Sentiment Chart Creation
+    function createSentimentChart(counts, canvasId) {
         const ctx = document.getElementById(canvasId).getContext('2d');
-
-        // Destroy the existing chart if it exists
-        if (currentChart) {
-            currentChart.destroy();
-        }
-
-        // Create a new chart
-        currentChart = new Chart(ctx, {
+        new Chart(ctx, {
             type: 'pie',
             data: {
                 labels: ['Positive', 'Negative', 'Neutral', 'Mixed'],
                 datasets: [{
                     label: 'Sentiment Analysis',
-                    data: [sentimentCounts.POSITIVE, sentimentCounts.NEGATIVE, sentimentCounts.NEUTRAL, sentimentCounts.MIXED],
+                    data: Object.values(counts),
                     backgroundColor: ['green', 'red', 'blue', 'gray'],
                     borderColor: ['darkgreen', 'darkred', 'darkblue', 'darkgray'],
                     borderWidth: 1
@@ -331,39 +116,30 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-
-    // Calculate sentiment percentage (optional for tooltips)
-    function calculateSentimentPercentage(count, total) {
-        return ((count / total) * 100).toFixed(2);
-    }
-
-    // Personalize recommendations (optional)
+    // Personalize recommendations
     function getPersonalizeRecommendations(userId) {
-        const apiGatewayUrl = 'https://za8k6zxf6c.execute-api.us-east-2.amazonaws.com/prod';
-        fetch(apiGatewayUrl, {
+        fetch('https://za8k6zxf6c.execute-api.us-east-2.amazonaws.com/prod', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ userId: userId })
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userId })
         })
             .then(response => response.json())
-            .then(data => {
-                console.log('Personalize Recommendations:', data);
-                // Process and display recommendations here
-            })
-            .catch(error => console.error('Error fetching Personalize recommendations:', error));
+            .then(data => displayPersonalizeRecommendations(data, userId))
+            .catch(error => console.error('Error:', error));
     }
 
-    // Event listener for the search bar
-    document.getElementById('searchBar').addEventListener('input', function (e) {
-        const searchTerm = e.target.value.toLowerCase();
-        const filteredUniversities = Object.keys(universitiesData).filter(universityName =>
-            universityName.toLowerCase().includes(searchTerm)
-        );
-        displayUniversityCards(filteredUniversities);
-    });
+    function displayPersonalizeRecommendations(data, universityName) {
+        const recommendationList = document.getElementById('recommended-universities');
+        recommendationList.innerHTML = data.itemList.map(item => `<li>${item.itemId}</li>`).join('');
+    }
 
-    // Initialize the application
+    // Search Event Handler
+    function attachSearchEventHandler() {
+        document.getElementById('searchBar').addEventListener('input', e => {
+            const searchTerm = e.target.value.toLowerCase();
+            displayUniversityCards(Object.keys(universitiesData).filter(name => name.toLowerCase().includes(searchTerm)));
+        });
+    }
+
     initializeApp();
 });
